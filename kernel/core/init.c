20f34e231ec61dd34421894910f0d5724ef56a6f
omrxdev <omarmohammed3147@gmail.com>
2026-07-05 08:08:32 +0300

    selinux_hide: improve hiding logic separately (not inline as before)
    Co-authored-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/core/init.c
+++ kernel/core/init.c
@@ -23,6 +23,7 @@
 #include <linux/susfs.h>
 #endif // #ifdef CONFIG_KSU_SUSFS
 #include "selinux/selinux.h"
+#include "feature/selinux_hide.h"
 #include "feature/adb_root.h"
 
 extern void __init ksu_lsm_hook_init(void);
@@ -114,7 +115,7 @@ int __init kernelsu_init(void)
 
 		apply_kernelsu_rules();
 		cache_sid();
-		ksu_selinux_hide_status_init();
+		ksu_selinux_hide_init();
 		setup_ksu_cred();
 
 		// Grant current process (ksud late-load) root
@@ -146,7 +147,7 @@ int __init kernelsu_init(void)
 
 		ksu_adb_root_init();
 
-		ksu_selinux_hide_status_init();
+		ksu_selinux_hide_init();
 
 		ksu_allowlist_init();
 
