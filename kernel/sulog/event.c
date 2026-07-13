f2c3c65d92787455eb6ee715c251d2ae5dc41112
omrxdev <omarmohammed3147@gmail.com>
2026-07-13 13:19:34 +0300

    kernel: feature: add compat header to provide strcpy_from_user_nofault

--- kernel/sulog/event.c
+++ kernel/sulog/event.c
@@ -14,6 +14,7 @@
 #include "infra/event_queue.h"
 #include "klog.h" // IWYU pragma: keep
 #include "sulog/event.h"
+#include "compat/kernel_compat.h"
 
 #define KSU_SULOG_MAX_QUEUED 256U
 #define KSU_SULOG_MAX_PAYLOAD_LEN 2048U
